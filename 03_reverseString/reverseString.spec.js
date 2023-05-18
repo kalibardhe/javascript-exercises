1d1b1fa58123864ec40451b8eaf76e9c3e719419
Paulo Cavalini <pcalexandre4@gmail.com>
2023-05-18 13:47:15 +0100

    Replace skip occurrences with test since it was removed by accident

--- 03_reverseString/reverseString.spec.js
+++ 03_reverseString/reverseString.spec.js
@@ -5,14 +5,14 @@ describe('reverseString', () => {
     expect(reverseString('hello')).toEqual('olleh');
   });
 
-  skip('reverses multiple words', () => {
+  test('reverses multiple words', () => {
     expect(reverseString('hello there')).toEqual('ereht olleh')
   })
 
-  skip('works with numbers and punctuation', () => {
+  test('works with numbers and punctuation', () => {
     expect(reverseString('123! abc!')).toEqual('!cba !321')
   })
-  skip('works with blank strings', () => {
+  test('works with blank strings', () => {
     expect(reverseString('')).toEqual('')
   })
 });

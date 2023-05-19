a8af9e31859ab8dcb3200e804a4f12d09f72bdc3
Paulo Cavalini <pcalexandre4@gmail.com>
2023-05-19 16:10:01 +0100

    Add content to both functions, pass first test

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fahrenheit) {
+  const celcius = (fahrenheit - 32) * 5 / 9;
+  return celcius;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celsius) {
+  const fahrenheit = (celsius * 9 / 5) + 32;
+  return fahrenheit;
 };
 
 // Do not edit below this line

0f249052d3facbf5659eb0f9cf8c1bde86d9291f
Paulo Cavalini <pcalexandre4@gmail.com>
2023-05-19 11:40:45 +0100

    Change the function so it works for multiple values

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,8 +1,11 @@
-const removeFromArray = function(array, value) {
-    const index = array.indexOf(value);
-    if (index !== -1) {
-        array.splice(index, 1);
-    } 
+const removeFromArray = function(array, values) {
+    for (let i = 0; i < values.length; i++) {
+        const value = values[i];
+        const index = array.indexOf(value);
+        if (index !== -1) {
+            array.splice(index, 1);
+        }
+    }
     return array;
 };
 
